9e0d8c03b8dce982939054c2527c0cc7778aa419
kanokpon <0102.getgallery@gmail.com>
2024-09-04 17:11:03 +0700

    my first commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<script setup>
+import MyCard from './components/MyCard.vue';
+import MyNav from './components/MyNav.vue';
+import items from './food.json'
+import Reference from './components/Reference.vue';
+</script>
+
+<template>
+    <div class="container">
+    <header>
+      <MyNav />
+    </header>
+    <main>
+      <h1>12 อารหารญี่ปุ่น</h1>
+      <div className="grid-container">
+        <MyCard v-bind="items[0]" />
+        <MyCard v-bind="items[1]" />
+        <MyCard v-bind="items[2]" />
+        <MyCard v-bind="items[3]" />
+        <MyCard v-bind="items[4]" />
+        <MyCard v-bind="items[5]" />
+        <MyCard v-bind="items[6]" />
+        <MyCard v-bind="items[7]" />
+        <MyCard v-bind="items[8]" />
+        <MyCard v-bind="items[9]" />
+        <MyCard v-bind="items[10]" />
+        <MyCard v-bind="items[11]" />
+      </div>
+    </main>
+    </div>
+    <footer>
+      <Reference />
+    </footer>
+</template>
+
+<style scoped>
+body{
+    text-align: center;
+}
+h1{
+  text-align: center;
+}
+.grid-container {
+    display: grid;
+    grid-column-gap: 0.5rem;
+    grid-row-gap: 0.5rem;
+}
+footer{
+    border: 1px solid rgb(255, 174, 0);
+    background-color: rgb(239, 255, 13);
+}
+@media (min-width: 576px) {
+  .grid-container { grid-template-columns: repeat(1, 1fr); }
+}
+@media (min-width: 992px) {
+  .grid-container { grid-template-columns: repeat(3, 1fr); }
+}
+
+</style>
+
+
+
+
+
---
+++ src/components/MyCard.vue
@@ -0,0 +1,37 @@
+<template lang="">
+  <div class="card">
+    <img :src="image" alt="name" style="width:100%">
+    <div class="container">
+      <h4><b>ชื่ออาหาร : {{name}}</b></h4>
+      <p>ชื่อญี่ปุ่น : {{namejapan}}</p>
+      <p>ชื่อไทย : {{namethai}}</p>
+      <p>รายละเอียด : {{detail}}</p>
+    </div>
+</div>
+</template>
+<script>
+export default{
+  props: ["name","namejapan","namethai","detail","image"]
+}
+</script>
+<style scoped>
+.card {
+box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+transition: 0.3s;
+width: 500px;
+text-align: center;
+
+}
+
+.card:hover {
+box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+padding: 2px 16px;
+}
+img{
+  width: 50px;
+  height: 400px;
+}
+</style>
